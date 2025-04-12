8787166fb03c9a5a74bdc2e6d1c725d3d3c56a55
hong0405 <wannab_cool@naver.com>
2025-04-12 22:30:39 +0900

    Updates

---
+++ precache-manifest.8f7823d779e74e0a23be9be7a8fe736f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ab0688b53dc761e0ebd04a4fd6fe24",
+    "url": "/digital-memorial/index.html"
+  },
+  {
+    "revision": "bd4a8c6101b3f4f8134d",
+    "url": "/digital-memorial/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2241419a0b62dc4e35b6",
+    "url": "/digital-memorial/static/js/2.f272b5a1.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/digital-memorial/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "bd4a8c6101b3f4f8134d",
+    "url": "/digital-memorial/static/js/main.27269f37.chunk.js"
+  },
+  {
+    "revision": "f8a9cfc3d97fbdd99c19",
+    "url": "/digital-memorial/static/js/runtime~main.f6a4f54e.js"
+  }
+]);
